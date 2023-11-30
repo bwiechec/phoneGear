0bc8e0068e94fad15f6652f759b79982b35de9dd
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2023-11-30 20:01:03 +0100

    Some minor changes.

--- src/app/adapters/page.tsx
+++ src/app/adapters/page.tsx
@@ -1,43 +1,6 @@
-import PagesList from "../components/productsList/productsList";
+import ProductsList from "../components/productsList/productsList";
 
-interface IProducts {
-  name: string;
-  price: number;
-  description: string;
-  imageUrl: string;
-  isBestseller: boolean;
-  isNew: boolean;
-  category: string;
-  currency: string;
-}
-
-interface ICategory {
-  id: string;
-  name: string;
-  parent: number;
-}
-
-interface IProductsData {
-  products: Array<IProducts>;
-  categories: Array<ICategory>;
-  currentCategory: ICategory | null;
-}
-
-interface IProductsObject {
-  [key: string]: IProducts;
-}
-
-interface ICategoryObject {
-  [key: string]: ICategory;
-}
-
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) {
+export default async function Page() {
   const categoryId = "2";
-  return <PagesList categoryId={categoryId} />;
+  return <ProductsList categoryId={categoryId} />;
 }
-
-export const dynamic = "force-dynamic";
--- src/app/cases/page.tsx
+++ src/app/cases/page.tsx
@@ -1,37 +1,6 @@
-import PagesList from "../components/productsList/productsList";
+import ProductsList from "../components/productsList/productsList";
 
-interface IProducts {
-  name: string;
-  price: number;
-  description: string;
-  imageUrl: string;
-  isBestseller: boolean;
-  isNew: boolean;
-  category: string;
-  currency: string;
-}
-
-interface ICategory {
-  id: string;
-  name: string;
-  parent: number;
-}
-
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) {
+export default async function Page() {
   const categoryId = "1";
-  return <PagesList categoryId={categoryId} />;
+  return <ProductsList categoryId={categoryId} />;
 }
-
-export const dynamic = "force-dynamic";
-
-// export async function generateStaticParams() {
-//   return [
-//     {
-//       subcategoryId: "",
-//     },
-//   ];
-// }
