0cf2211da8dad512a8a0624283db73ec88242bd8
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2023-11-30 15:01:06 +0100

    Some minor changes.

--- src/app/components/productsList/productsList.tsx
+++ src/app/components/productsList/productsList.tsx
@@ -2,6 +2,20 @@ import Link from "next/link";
 import ProductContainer from "../productContainer/productContainer";
 import styles from "./productsList.module.css";
 
+interface IProductsData {
+  products: Array<IProducts>;
+  categories: Array<ICategory>;
+  currentCategory: ICategory | null;
+}
+
+interface IProductsObject {
+  [key: string]: IProducts;
+}
+
+interface ICategoryObject {
+  [key: string]: ICategory;
+}
+
 interface IProducts {
   name: string;
   price: number;
@@ -23,8 +37,11 @@ interface IProductsData {
   products: Array<IProducts>;
   categories: Array<ICategory>;
   currentCategory: ICategory | null;
-  subcategoryId: string;
+}
+
+interface IProductsList {
   categoryId: string;
+  subcategoryId: string;
 }
 
 const pages = {
@@ -33,14 +50,11 @@ const pages = {
   ["3" as string]: "cables",
 };
 
-const ProductsList = ({
-  products,
-  categories,
-  currentCategory,
-  subcategoryId,
-  categoryId,
-}: IProductsData) => {
-  console.log(products.length);
+const ProductsList = async ({ categoryId, subcategoryId }: IProductsList) => {
+  console.log(categoryId);
+  console.log(subcategoryId);
+  const { products, categories, currentCategory }: IProductsData =
+    await getData({ categoryId, subcategoryId });
   return (
     <main className={styles.product_list}>
       <div className={styles.product_list_categories}>
@@ -91,6 +105,82 @@ const ProductsList = ({
       </div>
     </main>
   );
+};
+
+const getProductData = async (
+  categoryId: string,
+  subcategoryId: string | null
+) => {
+  const url = new URL(
+    "https://phonegear-302ea-default-rtdb.europe-west1.firebasedatabase.app/products.json"
+  );
+  if (subcategoryId) {
+    url.searchParams.append("orderBy", '"subcategory"');
+    url.searchParams.append("equalTo", `"${subcategoryId}"`);
+  }
+  url.searchParams.append("orderBy", '"category"');
+  url.searchParams.append("equalTo", `"${categoryId}"`);
+
+  const res = await fetch(url.toString());
+  const data: IProductsObject = await res.json();
+  let products = [];
+  for (const key in data) {
+    products.push({
+      name: data[key].name,
+      price: data[key].price,
+      description: data[key].description,
+      imageUrl: data[key].imageUrl,
+      isBestseller: data[key].isBestseller,
+      isNew: data[key].isNew,
+      category: data[key].category,
+      currency: data[key].currency,
+    });
+  }
+
+  return products;
+};
+
+const getCategoryData = async (categoryId: string) => {
+  const res = await fetch(
+    `https://phonegear-302ea-default-rtdb.europe-west1.firebasedatabase.app/categories.json?orderBy="parent"&equalTo="${categoryId}"`
+  );
+  const data: ICategoryObject = await res.json();
+  let products = [];
+  for (const key in data) {
+    products.push({
+      id: key,
+      name: data[key].name,
+      parent: data[key].parent,
+    });
+  }
+
+  products.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+  return products;
+};
+
+const getCurrentCategoryData = async (categoryId: string) => {
+  const res = await fetch(
+    `https://phonegear-302ea-default-rtdb.europe-west1.firebasedatabase.app/categories/${categoryId}.json`
+  );
+  const data: ICategory = await res.json();
+
+  return data;
+};
+
+const getData = async ({ categoryId, subcategoryId }: IProductsList) => {
+  const [products, categories, currentCategory]: [
+    Array<IProducts>,
+    Array<ICategory>,
+    ICategory
+  ] = await Promise.all([
+    getProductData(categoryId, subcategoryId),
+    getCategoryData(categoryId),
+    getCurrentCategoryData(categoryId),
+  ]);
+  const fixedCurrentCategory = categoryId === "0" ? null : currentCategory;
+
+  return { products, categories, currentCategory: fixedCurrentCategory };
 };
 
 export default ProductsList;
