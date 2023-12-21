201987941e8b328b3a94c7ae6c3c333f4642d804
Bartosz Wiecheć <32521016+bwiechec@users.noreply.github.com>
2023-12-21 20:37:37 +0100

    Additional basket components

--- src/app/components/BasketList/BasketList.tsx
+++ src/app/components/BasketList/BasketList.tsx
@@ -32,55 +32,63 @@ export default function BasketList() {
 
   return (
     <div className={styles.basket}>
-      <div className={styles.basket_list}>
-        {basket.map((basketItem) => {
-          return (
-            <BasketItem
-              key={basketItem.product.id}
-              basketItem={basketItem}
-              handleQuantityChange={handleQuantityChange}
-            />
-          );
-        })}
-      </div>
-      <div className={styles.basket_summary}>
-        SUMMARY
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "1rem",
-          }}
-        >
-          <span>Item value:</span>{" "}
-          <span>
-            {basketSummary.toLocaleString("en-US", {
-              style: "currency",
-              currency: basket[0].product.currency,
+      {basket?.length ? (
+        <>
+          <div className={styles.basket_list}>
+            (
+            {basket.map((basketItem) => {
+              return (
+                <BasketItem
+                  key={basketItem.product.id}
+                  basketItem={basketItem}
+                  handleQuantityChange={handleQuantityChange}
+                />
+              );
             })}
-          </span>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "1rem",
-          }}
-        >
-          <span>Delivery value:</span> <span>${123}</span>
-        </div>
-        <hr />
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "1rem",
-          }}
-        >
-          <span>Total value:</span> <span>${123}</span>
-        </div>
-        <Button className={styles.basket_summary_proceed}>Proceed</Button>
-      </div>
+            )
+          </div>
+          <div className={styles.basket_summary}>
+            SUMMARY
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "1rem",
+              }}
+            >
+              <span>Item value:</span>{" "}
+              <span>
+                {basketSummary.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: basket[0]?.product.currency ?? "USD",
+                })}
+              </span>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "1rem",
+              }}
+            >
+              <span>Delivery value:</span> <span>${123}</span>
+            </div>
+            <hr />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                gap: "1rem",
+              }}
+            >
+              <span>Total value:</span> <span>${123}</span>
+            </div>
+            <Button className={styles.basket_summary_proceed}>Proceed</Button>
+          </div>
+        </>
+      ) : (
+        <div>No products in basket</div>
+      )}
     </div>
   );
 }
